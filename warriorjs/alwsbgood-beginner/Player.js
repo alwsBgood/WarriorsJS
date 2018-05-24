32478356c6c8b1ec833cf589f6f413ec4ad510c5
alwsbgoood <d.chebotaru@kwebbl.com>
2018-05-24 17:09:03 +0300

    Level 5 done

--- warriorjs/alwsbgood-beginner/Player.js
+++ warriorjs/alwsbgood-beginner/Player.js
@@ -4,7 +4,7 @@ class Player {
   }
 
   isInjured(warrior) {
-    return warrior.health() < 15
+    return warrior.health() < 20
   }
 
   isTakingthDamage(warrior) {
@@ -12,16 +12,20 @@ class Player {
   }
 
   playTurn(warrior) {
+    const space = warrior.feel();
+    const unit = space.getUnit();
     if (
-      warrior.feel().isEmpty() &&
+      space.isEmpty() &&
       !this.isTakingthDamage(warrior) &&
       this.isInjured(warrior)
     ) {
       warrior.rest()
-    } else if (warrior.feel().isEmpty()) {
-      warrior.walk();
-    } else {
-      warrior.attack();
+    }  else {
+      space.isEmpty() ?
+        warrior.walk() :
+        unit.isEnemy() && !unit.isBound() ?
+          warrior.attack() :
+          warrior.rescue();
     }
 
     this.health = warrior.health();
